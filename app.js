aab8b33eca2e316424eefbfdae1e245cf069e3fd
Simon Knights <simon@redknight.org.uk>
2014-10-14 10:17:57 +0100

    General Initial Tidying

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var express = require('express');                      
-var path = require('path');                            
-var favicon = require('serve-favicon');                
-var logger = require('morgan');                        
-var cookieParser = require('cookie-parser');           
-var bodyParser = require('body-parser');               
-var simpledb = require('mongoose-simpledb');           
-var routes = require('./routes/index');                
-// Load                                                
-simpledb.init();                                       
-   
-var app = express();                                   
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var simpledb = require('mongoose-simpledb');
+var routes = require('./routes/index');
+// Loa
+simpledb.init();
+// Instantiate the express engine
+var app = express();
 
-// view engine setup                                   
-app.set('views', path.join(__dirname, 'views'));       
-app.set('view engine', 'jade');                        
- 
-// uncomment after placing your favicon in /public     
-//app.use(favicon(__dirname + '/public/favicon.ico')); 
-app.use(logger('dev'));                                
-app.use(bodyParser.json());                            
-app.use(bodyParser.urlencoded({ extended: false }));   
-app.use(cookieParser());                             
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
